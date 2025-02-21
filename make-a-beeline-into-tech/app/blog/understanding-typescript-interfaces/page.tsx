f0b896e444f49f8c3c6aa21cb9fc2eb34d4dc47d
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2025-02-21 10:44:44 +0000

    Update article
    Add content and styling.

--- make-a-beeline-into-tech/app/blog/understanding-typescript-interfaces/page.tsx
+++ make-a-beeline-into-tech/app/blog/understanding-typescript-interfaces/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import styles from "./page.module.css"
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Understanding TypeScript Interfaces",
@@ -16,7 +17,7 @@ export default function TypeScriptInterfacesPost() {
       </p>
       <h2 className={styles.heading}>Materials for this workshop</h2>
       <p>
-        If you want, you can get hands on with the workshop using a public GitHub repo. Please FORK the repo first before cloning it down locally. You can access it {""} 
+        If you would like to get hands on with the theory below, you can with this public GitHub repo. Please FORK the repo first before cloning it down locally. You can access it {""} 
         <a
             href="https://github.com/Maddy-bee/beeline-interface-workshop"
             target="_blank"
@@ -60,7 +61,7 @@ const workerBee: Bee = {
   canSting: true
 };
 
-describeBee(workerBee); // This will work perfectly!`}
+describeBee(workerBee);`}
         </code>
       </pre>
       <h2 className={styles.heading}>Extending Interfaces</h2>
@@ -140,14 +141,78 @@ const queenBee: Bee = {
 ]`}
         </code>
       </pre>
+      <h2 className={styles.heading}>Duck Typing</h2>
+      <p>You know the old saying, that if it quacks like a duck, it must be a duck? It can be applied within TypeScript as well. You can create an object like this one here:  </p>
+      <pre className={styles.codeBlock}>
+        <code>
+          {`const duckBee = {
+  species: "Carpenter Bee",
+  description: "A type of large, solitary bee",
+  pollinationEfficiency: 0.85,
+  endangeredStatus: "Least Concern",
+};`}
+        </code>
+      </pre>
+      <p>
+         The duckBee object is not explicity typed as Bee. However, it contains all of the properties expected, so it will be treated as the same in TypeScript. 
+      </p>
+      <h2 className={styles.heading}>Your Turn</h2>
+      <p>Try and apply the knowledge from this introduction by populating interfaces in this repository. You can also see an application of Duck typing as well. Scroll down on this page to find the answers.</p>
       <h2 className={styles.heading}>Conclusion</h2>
       <p>
         TypeScript interfaces are a fundamental tool for creating robust and maintainable code. They help catch errors
         early and make your code more self-documenting. Just as bees work together to maintain the structure of their
         hive, interfaces help maintain the structure and integrity of your TypeScript code ecosystem.
       </p>
-      
-
+      <Image src="/images/bee.webp" alt="Image of a bee" className={styles.image} width={400} height={300}/>
+      <h2 className={styles.heading}>Answers for the interface workshop</h2>
+      <p>Flowers</p>
+      <pre className={styles.codeBlock}>
+        <code>
+          {`interface Flower {
+  readonly name: string
+  scientificName: string
+  floweringSeason: string[]
+  colors: string[]
+  nectarRating?: number
+}`}
+        </code>
+      </pre>
+      <p>Butterflies</p>
+      <pre className={styles.codeBlock}>
+        <code>
+          {`interface Butterfly {
+  readonly name: string
+  wingspanCm: number
+  colors: string[]
+  migratoryPattern?: string
+}`}
+        </code>
+      </pre>
+      <p>Ladybirds</p>
+      <pre className={styles.codeBlock}>
+        <code>
+          {`interface Ladybird {
+  readonly species: string
+  spotCount: number
+  primaryColor: string
+  beneficialPrey: string[]
+  invasiveStatus?: boolean
+}
+`}
+ </code>
+ </pre>
+ <p>Birds</p>
+      <pre className={styles.codeBlock}>
+        <code>
+          {`export interface Bird {
+  readonly species: string
+  habitat: string[]
+  wingspan: number
+  diet: string[]
+}`}
+ </code>
+ </pre>
     </article>
   )
 }
